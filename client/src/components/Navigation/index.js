9f8e7162ae45a87f190b59321ffaa9d531592ef7
hugh18019 <hugh80082@gmail.com>
2022-03-30 03:00:08 -0700

    Toggle showing and hiding signin, signout button on logging in and logging out

--- client/src/components/Navigation/index.js
+++ client/src/components/Navigation/index.js
@@ -1,6 +1,7 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import moment from "moment";
+import { useSelector, useDispatch } from "react-redux";
 
 import "./navigation.style.css";
 
@@ -9,76 +10,69 @@ import { AiOutlineLogin } from "react-icons/ai";
 import { AiOutlineLogout } from "react-icons/ai";
 import { AiOutlineCoffee } from "react-icons/ai";
 import { AiOutlineMessage } from "react-icons/ai";
-import cpu from "../../assets/images/cpu2.svg";
 import clock from "../../assets/images/clock.svg";
 
-class Navigation extends Component {
-  constructor() {
-    super();
+function Navigation() {
+  // const [time, setTime] = useState();
+  // let timer = 0;
 
-    this.state = { time: 0 };
-    this.timer = 0;
-  }
+  // useEffect(() => {
+  //   timer = setInterval(() => {
+  //     setTime(moment().format("h:mm:ss a"));
+  //   }, 100);
 
-  componentDidMount() {
-    this.timer = setInterval(
-      () => this.setState({ time: moment().format("h:mm:ss a") }),
-      100
-    );
-  }
+  //   return () => clearTimeout(timer);
+  // }, []);
 
-  componentWillUnmount() {
-    clearTimeout(this.timer);
-  }
+  const logged_in = useSelector((state) => state.main.loggedIn);
 
-  render() {
-    const { children } = this.props;
-    const { time } = this.state;
-
-    return (
-      <div className="layout">
-        <header>
-          <nav className="nav">
-            <div className="name-tag">
-              {" "}
-              <img src={clock} alt="clock" /> {`${time}`}
-            </div>
-            <div className="nav-content-container">
-              <div id="navbar-content">
-                <ul>
+  return (
+    <div className="layout">
+      <header>
+        <nav className="nav">
+          <div className="name-tag">
+            {" "}
+            <img src={clock} alt="clock" />
+          </div>
+          <div className="nav-content-container">
+            <div id="navbar-content">
+              <ul>
+                {!logged_in && (
                   <li className="nav-item">
                     <Link to="/Login" className="link">
                       <AiOutlineLogin /> Signin
                     </Link>
                   </li>
+                )}
+                {logged_in && (
                   <li className="nav-item">
                     <Link to="/Logout" className="link">
                       <AiOutlineLogout /> Signout
                     </Link>
                   </li>
-                  <li className="nav-item">
-                    <Link to="/" className="link">
-                      <AiOutlineHome /> Home
-                    </Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link to="/Work" className="link">
-                      <AiOutlineCoffee /> Works
-                    </Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link to="/Contact" className="link">
-                      <AiOutlineMessage /> Contact
-                    </Link>
-                  </li>
-                </ul>
-              </div>
+                )}
+                <li className="nav-item">
+                  <Link to="/" className="link">
+                    <AiOutlineHome /> Home
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/Work" className="link">
+                    <AiOutlineCoffee /> Works
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/Contact" className="link">
+                    <AiOutlineMessage /> Contact
+                  </Link>
+                </li>
+              </ul>
             </div>
-          </nav>
-        </header>
-      </div>
-    );
-  }
+          </div>
+        </nav>
+      </header>
+    </div>
+  );
 }
 
 export default Navigation;
