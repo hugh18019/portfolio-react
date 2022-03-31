7f07a97c748650d4f5e32bd85d9c1b879b64881d
hugh18019 <hugh80082@gmail.com>
2022-03-30 23:11:43 -0700

    Removed jwk libraries configs in server.js

--- server/server.js
+++ server/server.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
 const path = require("path");
-var jwt = require("express-jwt");
-var jwks = require("jwks-rsa");
 
 // Resolvers populate data
 const { typeDefs, resolvers } = require("./schemas");
@@ -11,19 +9,6 @@ const db = require("./config/connection");
 const auth = require("./utils/auth");
 
 var PORT = process.env.PORT || 3001;
-
-var jwtCheck = jwt({
-  secret: jwks.expressJwtSecret({
-    cache: true,
-    rateLimit: true,
-    jwksRequestsPerMinute: 5,
-    jwksUri: "https://dev-3o0afhwh.us.auth0.com/.well-known/jwks.json",
-  }),
-  audience: "https://whispering-chamber-76792.herokuapp.com/",
-  issuer: "https://dev-3o0afhwh.us.auth0.com/",
-  algorithms: ["RS256"],
-});
-
 const app = express();
 
 const server = new ApolloServer({
@@ -39,7 +24,6 @@ server.start().then((res) => {
   // Middleware to convert objects served as objects
   app.use(express.urlencoded({ extended: false }));
   app.use(express.json());
-  app.use(jwtCheck);
 
   if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../client/build")));
@@ -47,10 +31,6 @@ server.start().then((res) => {
 
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../client/public/index.html"));
-  });
-
-  app.get("/authorized", function (req, res) {
-    res.send("Secured Resource");
   });
 
   db.once("open", () => {
