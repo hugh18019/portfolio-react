447d3879c8cbc49cc465d3d6e9f893aa7e190e1b
hugh18019 <hugh80082@gmail.com>
2022-04-03 04:22:03 -0700

    Added heroku postbuild script

--- client/src/index.js
+++ client/src/index.js
@@ -14,7 +14,7 @@ import Auth0ProviderWithHistory from "./auth/auth0-provider-with-history";
 const store = createStore(reducer);
 
 ReactDOM.render(
-  <Router basename="/index.html">
+  <Router basename="/">
     <Auth0ProviderWithHistory>
       <Provider store={store}>
         <App />
